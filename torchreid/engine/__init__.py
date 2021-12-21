c7489ac774657b9da7135b8380b51dfa688edd06
littleprince1121 <51303797+littleprince1121@users.noreply.github.com>
2021-12-21 17:00:59 +0800

    Add files via upload

---
+++ torchreid/engine/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import print_function, absolute_import
+
+from .image import ImageSoftmaxEngine, ImageTripletEngine, MultiModalImageSoftmaxEngine, Image3MEngine, ImageCMTEngine, ImageHCEngine
+
+from .video import VideoSoftmaxEngine, VideoTripletEngine
+from .engine import Engine
