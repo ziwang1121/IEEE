f53d259c4e33fbc10e71a1d183171f6c27872bfe
littleprince1121 <51303797+littleprince1121@users.noreply.github.com>
2021-12-21 10:19:46 +0800

    Add files via upload

---
+++ scripts/mainMultiModal.py
@@ -0,0 +1,241 @@
+import sys
+import time
+import os.path as osp
+import argparse
+import torch
+from torch.cuda.random import seed_all
+import torch.nn as nn
+import os
+import numpy as np
+import random
+
+import torchreid
+from torchreid.utils import (
+    Logger, check_isfile, set_random_seed, collect_env_info,
+    resume_from_checkpoint, load_pretrained_weights, compute_model_complexity
+)
+
+from scripts.default_config import (
+    imagedata_kwargs, optimizer_kwargs, videodata_kwargs, engine_run_kwargs,
+    get_default_config, lr_scheduler_kwargs
+)
+
+
+def seed_torch(seed):
+    seed = int(seed)
+
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.enabled = True
+
+
+def build_datamanager(cfg):
+    if cfg.data.type == 'image':
+        return torchreid.data.ImageDataManager(**imagedata_kwargs(cfg))
+    else:
+        return torchreid.data.VideoDataManager(**videodata_kwargs(cfg))
+
+
+def build_engine(cfg, datamanager, model, optimizer, scheduler):
+    if cfg.data.type == 'image':
+        if cfg.loss.name == 'softmax':
+            engine = torchreid.engine.MultiModalImageSoftmaxEngine(
+                datamanager,
+                model,
+                optimizer=optimizer,
+                scheduler=scheduler,
+                use_gpu=cfg.use_gpu,
+                label_smooth=cfg.loss.softmax.label_smooth
+            )
+        elif cfg.loss.name == 'triplet':
+            engine = torchreid.engine.ImageTripletEngine(
+                datamanager,
+                model,
+                optimizer=optimizer,
+                margin=cfg.loss.triplet_margin,
+                weight_t=cfg.loss.weight_t,
+                weight_x=cfg.loss.weight_x,
+                scheduler=scheduler,
+                use_gpu=cfg.use_gpu,
+                label_smooth=cfg.loss.softmax.label_smooth
+            )
+        elif cfg.loss.name == 'hcloss':
+            engine = torchreid.engine.ImageHCEngine(
+                datamanager,
+                model,
+                optimizer=optimizer,
+                margin=cfg.loss.triplet_margin,
+                weight_m=cfg.loss.weight_m,
+                weight_x=cfg.loss.weight_x,
+                scheduler=scheduler,
+                use_gpu=cfg.use_gpu,
+                label_smooth=cfg.loss.softmax.label_smooth
+            )
+        elif cfg.loss.name == 'margin':
+            engine = torchreid.engine.Image3MEngine(
+                datamanager,
+                model,
+                optimizer=optimizer,
+                margin=cfg.loss.ieee_margin,
+                weight_m=cfg.loss.weight_m,
+                weight_x=cfg.loss.weight_x,
+                scheduler=scheduler,
+                use_gpu=cfg.use_gpu,
+                label_smooth=cfg.loss.softmax.label_smooth
+            )
+        elif cfg.loss.name == 'CMT':
+            engine = torchreid.engine.ImageCMTEngine(
+                datamanager,
+                model,
+                optimizer=optimizer,
+                margin=cfg.loss.ieee_margin,
+                weight_m=cfg.loss.weight_m,
+                weight_x=cfg.loss.weight_x,
+                scheduler=scheduler,
+                use_gpu=cfg.use_gpu,
+                label_smooth=cfg.loss.softmax.label_smooth
+            )
+
+    else:
+        if cfg.loss.name == 'softmax':
+            engine = torchreid.engine.VideoSoftmaxEngine(
+                datamanager,
+                model,
+                optimizer=optimizer,
+                scheduler=scheduler,
+                use_gpu=cfg.use_gpu,
+                label_smooth=cfg.loss.softmax.label_smooth,
+                pooling_method=cfg.video.pooling_method
+            )
+
+        else:
+            engine = torchreid.engine.VideoTripletEngine(
+                datamanager,
+                model,
+                optimizer=optimizer,
+                margin=cfg.loss.triplet.margin,
+                weight_t=cfg.loss.triplet.weight_t,
+                weight_x=cfg.loss.triplet.weight_x,
+                scheduler=scheduler,
+                use_gpu=cfg.use_gpu,
+                label_smooth=cfg.loss.softmax.label_smooth
+            )
+
+    return engine
+
+
+def reset_config(cfg, args):
+    if args.root:
+        cfg.data.root = args.root
+    if args.sources:
+        cfg.data.sources = args.sources
+    if args.targets:
+        cfg.data.targets = args.targets
+    if args.transforms:
+        cfg.data.transforms = args.transforms
+
+
+def check_cfg(cfg):
+    if cfg.loss.name == 'triplet' and cfg.loss.weight_x == 0:
+        assert cfg.train.fixbase_epoch == 0, \
+            'The output of classifier is not included in the computational graph'
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        '--config-file', type=str, default='', help='path to config file'
+    )
+    parser.add_argument(
+        '--seed', type=int, default=1, help='seed'
+    )
+    parser.add_argument(
+        '-s',
+        '--sources',
+        type=str,
+        nargs='+',
+        help='source datasets (delimited by space)'
+    )
+    parser.add_argument(
+        '-t',
+        '--targets',
+        type=str,
+        nargs='+',
+        help='target datasets (delimited by space)'
+    )
+    parser.add_argument(
+        '--transforms', type=str, nargs='+', help='data augmentation'
+    )
+    parser.add_argument(
+        '--root', type=str, default=r'E:\code\AAAI2022\dataset', help='path to data root'
+    )
+    parser.add_argument(
+        'opts',
+        default=None,
+        nargs=argparse.REMAINDER,
+        help='Modify config options using the command-line'
+    )
+    args = parser.parse_args()
+
+    
+    cfg = get_default_config()
+    cfg.use_gpu = torch.cuda.is_available()
+    if args.config_file:
+        cfg.merge_from_file(args.config_file)
+    reset_config(cfg, args)
+    cfg.merge_from_list(args.opts)
+
+    check_cfg(cfg)
+    seed_torch(args.seed)
+
+    log_name = 'test.log' if cfg.test.evaluate else 'train.log'
+    log_time_name = time.strftime('%Y-%m-%d-%H-%M-%S-') + log_name
+    sys.stdout = Logger(osp.join(cfg.data.save_dir, log_time_name))
+
+    print('Show configuration\n{}\n'.format(cfg))
+    print("seed = ", args.seed)
+    datamanager = build_datamanager(cfg)
+
+    print('Building model: {}'.format(cfg.model.name))
+    model = torchreid.models.build_model(
+        name=cfg.model.name,
+        num_classes=datamanager.num_train_pids,
+        loss=cfg.loss.name,
+        pretrained=cfg.model.pretrained,
+        use_gpu=cfg.use_gpu
+    )
+
+    if cfg.model.load_weights and check_isfile(cfg.model.load_weights):
+        load_pretrained_weights(model, cfg.model.load_weights)
+
+    if cfg.use_gpu:
+        model = nn.DataParallel(model).cuda()
+
+    optimizer = torchreid.optim.build_optimizer(model, **optimizer_kwargs(cfg))
+    scheduler = torchreid.optim.build_lr_scheduler(
+        optimizer, **lr_scheduler_kwargs(cfg)
+    )
+
+    if cfg.model.resume and check_isfile(cfg.model.resume):
+        cfg.train.start_epoch = resume_from_checkpoint(
+            cfg.model.resume, model, optimizer=optimizer, scheduler=scheduler
+        )
+
+    print(
+        'Building {}-engine for {}-reid'.format(cfg.loss.name, cfg.data.type)
+    )
+
+    engine = build_engine(cfg, datamanager, model, optimizer, scheduler)
+    engine.run(**engine_run_kwargs(cfg))
+
+
+if __name__ == '__main__':
+    main()
