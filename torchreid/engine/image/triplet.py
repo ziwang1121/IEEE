a26c4bb62a9ce1011dc0f93bddc40b347d499502
littleprince1121 <51303797+littleprince1121@users.noreply.github.com>
2022-03-28 16:10:52 +0800

    Update triplet.py

--- torchreid/engine/image/triplet.py
+++ torchreid/engine/image/triplet.py
@@ -103,113 +103,40 @@ class ImageTripletEngine(Engine):
                 imgs[i] = imgs[i].cuda()
             pids = pids.cuda()
 
-        outputs_R, outputs_N, outputs_T, features_R, features_N, features_T, features_all = self.model(imgs)
+        outputs, features = self.model(imgs)
         if self.weight_t > 0:
             # loss_t_all = self.criterion_t(features_all, pids)
-            loss_t_R = self.criterion_t(features_R, pids)
-            loss_t_N = self.criterion_t(features_N, pids)
-            loss_t_T = self.criterion_t(features_T, pids)
+            loss_t = self.criterion_t(features, pids)
+
         
-        loss_t = min(loss_t_R, loss_t_N, loss_t_T) * 10
         # print(loss_t, loss_t_R, loss_t_N, loss_t_T)
         
 
-        loss_R = self.compute_loss(self.criterion_x, outputs_R, pids)
-        loss_N = self.compute_loss(self.criterion_x, outputs_N, pids)
-        loss_T = self.compute_loss(self.criterion_x, outputs_T, pids)
+        loss_R = self.compute_loss(self.criterion_x, outputs, pids)
 
-        loss = loss_R + loss_N + loss_T + loss_t
+        loss = loss_R + loss_t
         # loss = loss_R
         
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
         
-        acc_R = 0; acc_N = 0; acc_T = 0
-        if isinstance(outputs_R, (tuple, list)):
-            for i in range(len(outputs_R)):
-                acc_R += metrics.accuracy(outputs_R[i], pids)[0]
-                acc_N += metrics.accuracy(outputs_N[i], pids)[0]
-                acc_T += metrics.accuracy(outputs_T[i], pids)[0]
+        acc = 0
+        if isinstance(outputs, (tuple, list)):
+            for i in range(len(outputs)):
+                acc += metrics.accuracy(outputs[i], pids)[0]
                 # print(acc)
-            acc_R /= len(outputs_R)
-            acc_N /= len(outputs_N)
-            acc_T /= len(outputs_T)
+            acc /= len(outputs)
         else:
-            acc_R = metrics.accuracy(outputs_R, pids)[0]
-            acc_N = metrics.accuracy(outputs_N, pids)[0]
-            acc_T = metrics.accuracy(outputs_T, pids)[0]
+            acc = metrics.accuracy(outputs, pids)[0]
+
 
 
         loss_summary = {
             'loss_all': loss.item(),
             'loss_t': loss_t.item(),
-            'acc_R': acc_R.item(),
-            # 'loss_t_R': loss_t_R.item(),
-            'loss_R': loss_R.item(),
-            'acc_N': acc_N.item(),
-            # 'loss_t_N': loss_t_N.item(),
-            'loss_N': loss_N.item(),
-            'acc_T': acc_T.item(),
-            # 'loss_t_T': loss_t_T.item(),
-            'loss_T': loss_T.item(),
+            'acc': acc.item(),
         }
 
 
         return loss_summary
-
-    # def forward_backward(self, data):
-    #     imgs, pids, timeids = self.parse_data_for_train(data)
-
-    #     if self.use_gpu:
-    #         for i in range(len(imgs)):
-    #             imgs[i] = imgs[i].cuda()
-    #         pids = pids.cuda()
-
-    #     outputs_R, outputs_N, outputs_T, features_R, features_N, features_T, features_all = self.model(imgs)
-
-    #     if self.weight_t > 0:
-    #         loss_t = 0.01
-
-    #     loss_R = self.compute_loss(self.criterion_x, outputs_R, pids)
-    #     loss_N = self.compute_loss(self.criterion_x, outputs_N, pids)
-    #     loss_T = self.compute_loss(self.criterion_x, outputs_T, pids)
-
-    #     loss = loss_R + loss_N + loss_T
-    #     # loss = loss_R
-        
-    #     self.optimizer.zero_grad()
-    #     loss.backward()
-    #     self.optimizer.step()
-        
-    #     acc_R = 0; acc_N = 0; acc_T = 0
-    #     if isinstance(outputs_R, (tuple, list)):
-    #         for i in range(len(outputs_R)):
-    #             acc_R += metrics.accuracy(outputs_R[i], pids)[0]
-    #             acc_N += metrics.accuracy(outputs_N[i], pids)[0]
-    #             acc_T += metrics.accuracy(outputs_T[i], pids)[0]
-    #             # print(acc)
-    #         acc_R /= len(outputs_R)
-    #         acc_N /= len(outputs_N)
-    #         acc_T /= len(outputs_T)
-    #     else:
-    #         acc_R = metrics.accuracy(outputs_R, pids)[0]
-    #         acc_N = metrics.accuracy(outputs_N, pids)[0]
-    #         acc_T = metrics.accuracy(outputs_T, pids)[0]
-
-
-    #     loss_summary = {
-    #         'loss_all': loss.item(),
-    #         # 'loss_t': loss_t.item(),
-    #         'loss_R': loss_R.item(),
-    #         'acc_R': acc_R.item(),
-    #         'loss_N': loss_N.item(),
-    #         'acc_N': acc_N.item(),
-    #         'loss_T': loss_T.item(),
-    #         'acc_T': acc_T.item(),
-    #     }
-    #     loss = 0
-    #     loss_summary = {}
-
-
-    #     return loss_summary
