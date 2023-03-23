8a6947a7911b9b1f886acb4e3fee2a9faad88222
littleprince1121 <51303797+littleprince1121@users.noreply.github.com>
2023-03-23 21:17:47 +0800

    Delete torchreid directory

--- torchreid/engine/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function, absolute_import
-
-from .image import ImageSoftmaxEngine, ImageTripletEngine, MultiModalImageSoftmaxEngine, Image3MEngine, ImageCMTEngine, ImageHCEngine
-
-from .video import VideoSoftmaxEngine, VideoTripletEngine
-from .engine import Engine
